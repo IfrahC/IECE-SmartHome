53cd6fac74afc949a1c10f1775b8de091c6e946f
Ifrah <ifrahchishti2004@gmail.com>
2025-11-06 16:15:59 +0500

    added code for gate gui

--- gui.py
+++ gui.py
@@ -17,8 +17,8 @@ container = Box(app, layout="grid")
 Text(container, text="Humidity", grid=[0, 1], align="left", size=label_size, color=label_color)
 humidity_val = Text(container, text="0%", grid=[1, 1], align="right", size=value_size, color=value_color)
 
-Text(container, text="Servo Status", grid=[0, 2], align="left", size=label_size, color=label_color)
-servoStatus = Text(container, text="Closed", grid=[1, 2], align="right", size=value_size, color=value_color)
+Text(container, text="Gate Status", grid=[0, 2], align="left", size=label_size, color=label_color)
+gateStatus = Text(container, text="Closed", grid=[1, 2], align="right", size=value_size, color=value_color)
 
 Text(container, text="PIR Status", grid=[0, 3], align="left", size=label_size, color=label_color)
 pirStatus = Text(container, text="No Motion", grid=[1, 3], align="right", size=value_size, color=value_color)
